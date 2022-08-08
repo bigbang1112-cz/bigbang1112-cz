7a58783c1929375b3beaba9c1197051d5fd998e5
Petr Pivoňka <petrpiv1@gmail.com>
2022-08-09 00:43:47 +0200

    Add /voice

---
+++ BigBang1112.BongoBot3/Commands/VoiceCommand.cs
@@ -0,0 +1,30 @@
+﻿using BigBang1112.DiscordBot;
+using BigBang1112.DiscordBot.Data;
+
+namespace BigBang1112.BongoBot3.Commands;
+
+[DiscordBotCommand("voice")]
+public partial class VoiceCommand : DiscordBotCommand
+{
+    public VoiceCommand(DiscordBotService discordBotService) : base(discordBotService)
+    {
+        
+    }
+
+    public override async Task<DiscordBotMessage> ExecuteAsync(SocketInteraction slashCommand)
+    {
+        if (slashCommand.User is not SocketGuildUser guildUser)
+        {
+            return new DiscordBotMessage("You're not executing this command on a server", ephemeral: true);
+        }
+
+        if (guildUser.VoiceChannel is null)
+        {
+            return new DiscordBotMessage("You're not in a voice channel", ephemeral: true);
+        }
+
+        await guildUser.VoiceChannel.ConnectAsync(external: true);
+
+        return new DiscordBotMessage("I joined");
+    }
+}
