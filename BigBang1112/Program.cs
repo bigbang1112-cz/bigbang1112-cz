0bd41fed8765a9a037c955c5630168586a78128e
Petr Pivoňka <petrpiv1@gmail.com>
2022-08-09 06:30:08 +0200

    Fix serilog

--- BigBang1112/Program.cs
+++ BigBang1112/Program.cs
@@ -7,6 +7,8 @@ var assembly = typeof(Program).Assembly;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseEssentials();
+
 var config = builder.Configuration;
 
 var options = new EssentialsOptions
